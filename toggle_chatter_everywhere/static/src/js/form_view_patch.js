75cd91f23fe4f16646c269bac6d0c88bdd033266
Ray Carnes <ray@odoo.com>
2026-07-31 14:00:02 +0200

    Delete toggle_chatter_everywhere directory

--- toggle_chatter_everywhere/static/src/js/form_view_patch.js
+++
@@ -1,6 +0,0 @@
-/** @odoo-module **/
-
-import { formView } from "@web/views/form/form_view";
-import { ChatterToggleControlPanel } from "./chatter_toggle_control_panel";
-
-formView.ControlPanel = ChatterToggleControlPanel;
